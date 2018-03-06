8791dd3fc38bec5737a6f529520213c6de99a504
ezhh <owlecho@live.com>
2018-03-06 00:53:20 +0000

    Add more colours

--- init.lua
+++ init.lua
@@ -1,32 +1,42 @@
 
-minetest.register_node("butterflies:butterfly", {
-	description = "Butterfly",
-	drawtype = "plantlike",
-	tiles = {{
-		name = "butterflies_butterfly_animated.png",
-		animation = {
-			type = "vertical_frames",
-			aspect_w = 16,
-			aspect_h = 16,
-			length = 3
-		},
-	}},
-	inventory_image = "butterflies_butterfly.png",
-	wield_image =  "butterflies_butterfly.png",
-	waving = 1,
-	paramtype = "light",
-	sunlight_propagates = true,
-	buildable_to = true,
-	walkable = false,
-	groups = {catchable = 1},
-	selection_box = {
-		type = "fixed",
-		fixed = {-0.1, -0.1, -0.1, 0.1, 0.1, 0.1},
-	},
-	floodable = true,
-	on_flood = function(pos, oldnode, newnode)
-		minetest.add_item(pos, "butterflies:butterfly 1")
-	end
-})
+local butter_list = {
+	{"white", "White"},
+	{"red", "Red"},
+	{"violet", "Violet"}
+}
 
+for i in ipairs (butter_list) do
+	local name = butter_list[i][1]
+	local desc = butter_list[i][2]
+
+	minetest.register_node("butterflies:butterfly_"..name, {
+		description = desc.." Butterfly",
+		drawtype = "plantlike",
+		tiles = {{
+			name = "butterflies_butterfly_"..name.."_animated.png",
+			animation = {
+				type = "vertical_frames",
+				aspect_w = 16,
+				aspect_h = 16,
+				length = 3
+			},
+		}},
+		inventory_image = "butterflies_butterfly_"..name..".png",
+		wield_image =  "butterflies_butterfly_"..name..".png",
+		waving = 1,
+		paramtype = "light",
+		sunlight_propagates = true,
+		buildable_to = true,
+		walkable = false,
+		groups = {catchable = 1},
+		selection_box = {
+			type = "fixed",
+			fixed = {-0.1, -0.1, -0.1, 0.1, 0.1, 0.1},
+		},
+		floodable = true,
+		on_flood = function(pos, oldnode, newnode)
+			minetest.add_item(pos, "butterflies:butterfly_"..name.."1")
+		end
+	})
+end
 
