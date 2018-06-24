fe1e4da5f9c32f9bcffc67187403cdb84f7d018d
ezhh <owlecho@live.com>
2018-06-24 14:47:02 +0100

    Adjust sidelen

--- init.lua
+++ init.lua
@@ -98,7 +98,7 @@ minetest.register_decoration({
 	deco_type = "simple",
 	place_on = {"default:dirt_with_grass"},
 	place_offset_y = 1,
-	sidelen = 16,
+	sidelen = 80,
 	fill_ratio = 0.005,
 	biomes = {"grassland", "deciduous_forest"},
 	y_max = 300,
